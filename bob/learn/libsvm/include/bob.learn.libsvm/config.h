27874dee1f8032bee5f2245e24efd5fd396ded3b
Manuel Guenther <manuel.guenther@idiap.ch>
2015-05-22 10:26:13 +0200

    Applied new versioning system

--- bob/learn/libsvm/include/bob.learn.libsvm/config.h
+++ bob/learn/libsvm/include/bob.learn.libsvm/config.h
@@ -11,4 +11,33 @@
 /* Macros that define versions and important names */
 #define BOB_LEARN_LIBSVM_API_VERSION 0x0200
 
+#ifdef BOB_IMPORT_VERSION
+
+  /***************************************
+  * Here we define some functions that should be used to build version dictionaries in the version.cpp file
+  * There will be a compiler warning, when these functions are not used, so use them!
+  ***************************************/
+
+  #include <Python.h>
+  #include <boost/preprocessor/stringize.hpp>
+  #include <svm.h>
+
+  /**
+   * Describes the libsvm version
+   */
+  static PyObject* get_libsvm_version() {
+    boost::format s("%d.%d");
+    s % (LIBSVM_VERSION / 100) % (LIBSVM_VERSION % 100);
+    return Py_BuildValue("s", s.str().c_str());
+  }
+
+  /**
+   * bob.learn.libsvm c/c++ api version
+   */
+  static PyObject* bob_learn_libsvm_version() {
+    return Py_BuildValue("{ss}", "api", BOOST_PP_STRINGIZE(BOB_LEARN_LIBSVM_API_VERSION));
+  }
+
+#endif // BOB_IMPORT_VERSION
+
 #endif /* BOB_LEARN_LIBSVM_CONFIG_H */
--- bob/learn/libsvm/main.cpp
+++ bob/learn/libsvm/main.cpp
@@ -13,6 +13,7 @@
 #endif
 #include <bob.blitz/capi.h>
 #include <bob.blitz/cleanup.h>
+#include <bob.core/api.h>
 #include <bob.io.base/api.h>
 
 static PyMethodDef module_methods[] = {
@@ -131,17 +132,9 @@ static PyObject* create_module (void) {
   if (c_api_object) PyModule_AddObject(m, "_C_API", c_api_object);
 
   /* imports dependencies */
-  if (import_bob_blitz() < 0) {
-    PyErr_Print();
-    PyErr_Format(PyExc_ImportError, "cannot import `%s'", BOB_EXT_MODULE_NAME);
-    return 0;
-  }
-
-  if (import_bob_io_base() < 0) {
-    PyErr_Print();
-    PyErr_Format(PyExc_ImportError, "cannot import `%s'", BOB_EXT_MODULE_NAME);
-    return 0;
-  }
+  if (import_bob_blitz() < 0) return 0;
+  if (import_bob_core_logging() < 0) return 0;
+  if (import_bob_io_base() < 0) return 0;
 
   return Py_BuildValue("O", m);
 }
